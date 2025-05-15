71da7f46f6561c1e9987dcd4732a1d0447f21da8
FMT86 <mtovar.francisco@gmail.com>
2025-05-16 00:30:58 +0200

    Initial import

---
+++ src/main/java/com/example/musicat/MusiCatApplication.java
@@ -0,0 +1,12 @@
+package com.example.musicat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiCatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MusiCatApplication.class, args);
+	}
+
+}
