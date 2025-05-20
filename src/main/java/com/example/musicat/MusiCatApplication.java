d1f3320a918793195dec64db33dada6d1793ca7c
FMT86 <mtovar.francisco@gmail.com>
2025-05-20 10:19:15 +0200

    Initial commit: set up Spring Boot project with Gradle

---
+++ src/main/java/com/example/musicat/MusiCatApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusiCatApplication.class, args);
+    }
+
+}
